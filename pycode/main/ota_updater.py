58d2b542e14c15a9b0201fc397582379e176a1da
betpagal <brian.travis@gmail.com>
2020-12-20 16:59:10 -0500

    Synchronizing with code on feather

--- pycode/main/ota_updater.py
+++ pycode/main/ota_updater.py
@@ -1,10 +1,10 @@
-#import os
-#import gc
+import os
+import gc
 import supervisor
 import board
 import busio
 from digitalio import DigitalInOut
-import main.lib.adafruit_requests as requests
+import adafruit_requests as requests
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
 from adafruit_esp32spi import adafruit_esp32spi
 from main.secrets import secrets
@@ -47,17 +47,6 @@ class OTAUpdater:
                 continue
         print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
         print("My IP address is", esp.pretty_ip(esp.ip_address))
-
-#        print('in using_network')
-#        import network
-#        sta_if = network.WLAN(network.STA_IF)
-#        if not sta_if.isconnected():
-#            print('connecting to network...')
-#            sta_if.active(True)
-#            sta_if.connect(ssid, password)
-#            while not sta_if.isconnected():
-#                pass
-#        print('network config:', sta_if.ifconfig())
 
     def check_for_update_to_install_during_next_reboot(self):
         print('in check_for_update_to_install_during_next_reboot')
@@ -137,7 +126,6 @@ class OTAUpdater:
             is_dir = entry[1] == 0x4000
             if is_dir:
                 self.rmtree(directory + '/' + entry[0])
-
             else:
                 os.remove(directory + '/' + entry[0])
         os.rmdir(directory)
@@ -151,7 +139,7 @@ class OTAUpdater:
             return version
         return '0.0'
 
-    def get_latest_version(self):
+    def get_latest_version(self): # this returns the tag of the latest version
         print('in get_latest_version')
         OTAUpdater.using_network()
         dest_path = self.github_repo + '/releases/latest'
@@ -165,27 +153,35 @@ class OTAUpdater:
         return version
 
     def download_all_files(self, root_url, version):
-        #file_list = self.http_client.get(root_url + '?ref=refs/tags/' + version)
         dest_path = root_url + '?ref=refs/tags/' + version
         print('dest_path: ', dest_path)
         file_list = requests.get(dest_path)
         for file in file_list.json():
             if file['type'] == 'file':
+                print('found type:file: ', file['download_url'])
+                print('found file in pycode')
                 download_url = file['download_url']
                 download_path = self.modulepath('next/' + file['path'].replace(self.main_dir + '/', ''))
-                self.download_file(download_url.replace('refs/tags/', ''), download_path)
+                pyfile = download_url.replace('refs/tags/', ''), download_path
+                print('pyfile: ', pyfile)
+                if 'pycode/main' in pyfile:
+                    self.download_file(pyfile)
             elif file['type'] == 'dir':
+                print('found directory: ', file['path'])
+                print('found pycode directory')
                 path = self.modulepath('next/' + file['path'].replace(self.main_dir + '/', ''))
-                os.mkdir(path)
-                self.download_all_files(root_url + '/' + file['name'], version)
+                if 'pycode/main' in path:
+                    os.mkdir(path)
+                    self.download_all_files(root_url + '/' + file['name'], version)
 
         file_list.close()
  
     def download_file(self, url, path):
-        print('url: ', url, '\path: ', path)
+        print('in download_file. url: ', url, '\n    path: ', path)
         with open(path, 'w') as outfile:    
             response = requests.get(url)
             try:
+                print('Writing response.text')
                 outfile.write(response.text) 
             except RuntimeError as e:
                 print('error caught while downloading file')
@@ -194,147 +190,10 @@ class OTAUpdater:
                 print(e)          # __str__ allows args to be printed directly,
                                   # but may be overridden in exception subclasses
             finally:
+                print('in finally. Closing and GC')
                 response.close()
                 outfile.close()
                 gc.collect()
 
     def modulepath(self, path):
         return self.module + '/' + path if self.module else path
-
-
-# class Response:
-
-#     def __init__(self, f):
-#         self.raw = f
-#         self.encoding = 'utf-8'
-#         self._cached = None
-
-#     def close(self):
-#         if self.raw:
-#             self.raw.close()
-#             self.raw = None
-#         self._cached = None
-
-#     @property
-#     def content(self):
-#         if self._cached is None:
-#             try:
-#                 self._cached = self.raw.read()
-#             finally:
-#                 self.raw.close()
-#                 self.raw = None
-#         return self._cached
-
-#     @property
-#     def text(self):
-#         return str(self.content, self.encoding)
-
-#     def json(self):
-#         import ujson
-#         return ujson.loads(self.content)
-
-
-# class HttpClient:
-
-#     def __init__(self, headers={}):
-#         print('in HttpClient.__init__')
-#         self._headers = headers
-
-#     def request(self, method, url, data=None, json=None, headers={}, stream=None):
-#         print('in HttpClient.request')
-#         def _write_headers(sock, _headers):
-#             print('in HttpClient.request._write_headers')
-#             for k in _headers: 
-#                 sock.write(b'{}: {}\r\n'.format(k, _headers[k]))
-
-#         try:
-#             proto, dummy, host, path = url.split('/', 3)
-#         except ValueError:
-#             proto, dummy, host = url.split('/', 2)
-#             path = ''
-#         if proto == 'http:':
-#             port = 80
-#         elif proto == 'https:':
-#             import ussl
-#             port = 443
-#         else:
-#             raise ValueError('Unsupported protocol: ' + proto)
-
-#         if ':' in host:
-#             host, port = host.split(':', 1)
-#             port = int(port)
-
-#         ai = usocket.getaddrinfo(host, port, 0, usocket.SOCK_STREAM)
-#         ai = ai[0]
-
-#         s = usocket.socket(ai[0], ai[1], ai[2])
-#         try:
-#             s.connect(ai[-1])
-#             if proto == 'https:':
-#                 s = ussl.wrap_socket(s, server_hostname=host)
-#             s.write(b'%s /%s HTTP/1.0\r\n' % (method, path))
-#             if not 'Host' in headers:
-#                 s.write(b'Host: %s\r\n' % host)
-#             # Iterate over keys to avoid tuple alloc
-#             _write_headers(s, self._headers)
-#             _write_headers(s, headers)
-
-#             # add user agent
-#             s.write('User-Agent')
-#             s.write(b': ')
-#             s.write('MicroPython OTAUpdater')
-#             s.write(b'\r\n')
-#             if json is not None:
-#                 assert data is None
-#                 import ujson
-#                 data = ujson.dumps(json)
-#                 s.write(b'Content-Type: application/json\r\n')
-#             if data:
-#                 s.write(b'Content-Length: %d\r\n' % len(data))
-#             s.write(b'\r\n')
-#             if data:
-#                 s.write(data)
-
-#             l = s.readline()
-#             # print(l)
-#             l = l.split(None, 2)
-#             status = int(l[1])
-#             reason = ''
-#             if len(l) > 2:
-#                 reason = l[2].rstrip()
-#             while True:
-#                 l = s.readline()
-#                 if not l or l == b'\r\n':
-#                     break
-#                 # print(l)
-#                 if l.startswith(b'Transfer-Encoding:'):
-#                     if b'chunked' in l:
-#                         raise ValueError('Unsupported ' + l)
-#                 elif l.startswith(b'Location:') and not 200 <= status <= 299:
-#                     raise NotImplementedError('Redirects not yet supported')
-#         except OSError:
-#             s.close()
-#             raise
-
-#         resp = Response(s)
-#         resp.status_code = status
-#         resp.reason = reason
-#         return resp
-
-#     def head(self, url, **kw):
-#         return self.request('HEAD', url, **kw)
-
-#     def get(self, url, **kw):
-#         return self.request('GET', url, **kw)
-
-#     def post(self, url, **kw):
-#         return self.request('POST', url, **kw)
-
-#     def put(self, url, **kw):
-#         return self.request('PUT', url, **kw)
-
-#     def patch(self, url, **kw):
-#         return self.request('PATCH', url, **kw)
-
-#     def delete(self, url, **kw):
-#         return self.request('DELETE', url, **kw)
